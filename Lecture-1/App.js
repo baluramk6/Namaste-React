3f12494416bd5678b42ecb22efb353c908cb9532
baluramk6 <baluramk6@gmail.com>
2023-02-11 09:19:11 +0530

    Namaste React Lecture-1

---
+++ Lecture-1/App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1", { id: "header1", key: "1" }, "Heading-1")
+const heading2 = React.createElement("h2", { id: "header2", key: "2" }, "Heading-2")
+const input = React.createElement("input", { id: "input", key: "3", placeholder: "Enter name" },)
+const div = React.createElement('div', {}, [heading, heading2, input])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(div)
